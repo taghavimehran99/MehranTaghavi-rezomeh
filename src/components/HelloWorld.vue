0d722d212685d57f0b22b2afdaec1fd3fe964ccd
Your Name <you@example.com>
2025-11-14 01:10:28 +0330

    add titiel and fix imgs in files

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,37 +1,26 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-  <div class="greetings">
-    
-
-    <h1>yyyyyyyyyyyyyyy</h1>
-    
+  <div>
+    <img class="hero-picture" src="@/assets/img/image.png" alt="my-pictuer-rezomeh" />
+  </div>
+  <div class="my-name">
+    <h1>Mehran</h1>
+    <h1 class="name-titiel">Taghavi</h1>
   </div>
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
+.hero-picture {
+  width: 200px;
+  border-radius: 100px;
 }
 
-h3 {
-  font-size: 1.2rem;
+.my-name{
+    
 }
 
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
+.name-titiel{
+  margin: 30px 0px 0px 80px;
 }
 </style>
