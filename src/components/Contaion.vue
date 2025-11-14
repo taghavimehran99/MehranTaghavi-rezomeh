6085dc677da6d47a164b160ef21938e0360686a9
Your Name <you@example.com>
2025-11-15 00:11:17 +0330

    add layout folder and file in project

---
+++ src/components/Contaion.vue
@@ -0,0 +1,32 @@
+<script setup></script>
+<template>
+  <div>
+  </div>
+  <div class="my-name">
+    <h2>About Me</h2>
+    <h4>
+      Born on January 10, 1999, I graduated from Sohrudi University with a GPA of 17.75 in 2019. In
+      addition to my studies, I gained a variety of work experiences during and after my university
+      years that helped me develop my skills and practical perspective in related fields.After
+      completing my studies, I completed my 18-month military service and entered the job market
+      with a focus on learning and progress. This path gave me the opportunity to combine my
+      theoretical knowledge and practical experience and take steps towards achieving my career
+      goals.
+    </h4>
+  </div>
+</template>
+<style scoped>
+.hero-picture {
+  width: 200px;
+  border-radius: 100px;
+}
+
+.first-name {
+  font-weight: 700;
+}
+
+.last-name {
+  margin: 30px 0px 0px 80px;
+  font-weight: 700;
+}
+</style>
---
+++ src/layout/home.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Header from '@/components/Header.vue'
+import Contaion from '@/components/Contaion.vue'
+</script>
+<template>
+  <Header />
+  <Contaion />
+</template>
+<style scoped></style>
