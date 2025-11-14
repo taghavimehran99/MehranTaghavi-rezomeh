0fa77597111a3161861432ae78d2dfbba4346917
Your Name <you@example.com>
2025-11-15 00:29:23 +0330

    add component email in project

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
-<script setup>
-</script>
+<script setup></script>
 <template>
   <RouterView />
 </template>
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ src/components/Email.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+<template>
+  <div></div>
+  <div class="my-name">
+    <h2>taghavimehran99@gmail.com</h2>
+    
+  </div>
+</template>
+<style scoped></style>
