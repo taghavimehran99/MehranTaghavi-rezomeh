a0fdc95030dbbc963ea76d528aea69637334f58e
Your Name <you@example.com>
2025-11-15 00:02:14 +0330

    fix name class and style in project

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,11 @@
 <script setup></script>
 <template>
   <div>
-    <img class="hero-picture" src="@/assets/img/image.png" alt="my-pictuer-rezomeh" />
+    <img class="hero-picture" src="@/assets/img/image.png" alt="pictuer-mehran-taghavi-rezomeh" />
   </div>
   <div class="my-name">
-    <h1>Mehran</h1>
-    <h1 class="name-titiel">Taghavi</h1>
+    <h1 class="first-name">Mehran</h1>
+    <h1 class="last-name">Taghavi</h1>
   </div>
 </template>
 <style scoped>
@@ -14,7 +14,12 @@
   border-radius: 100px;
 }
 
-.name-titiel{
+.first-name {
+  font-weight: 700;
+}
+
+.last-name {
   margin: 30px 0px 0px 80px;
+  font-weight: 700;
 }
 </style>
